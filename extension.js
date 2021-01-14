a295735bd05508b48784ce964fb60e3ebb9f6d9e
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-01-14 14:28:14 -0500

    set up for beta publishing and TA testing

--- extension.js
+++ extension.js
@@ -1,23 +1,40 @@
-
-const { start } = require('repl');
 const vscode = require('vscode');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+const activate = async (context) => {
 
-	const hour = (1000 * 60 * 60);
+	const oneMinute = (1000 * 60)
+	const timeIncrement = (oneMinute * 15);
 
 	const startTime = new Date();
 	const localTime = startTime.toLocaleTimeString();
+
+	const response = await vscode.window.showInformationMessage('Welcome to beHuman! Would you like to be reminded to take breaks today?', 'Yes', 'No');
 	
+	if(response === 'Yes') {
+		vscode.window.showInformationMessage('Have a great day!');
+		
+		setInterval(() => {
+			const lapTime = new Date();
+
+			const incrementOfTime = Math.round((lapTime - startTime) / 60000);
+			
+
+			vscode.window.showInformationMessage(`You have been working for ${incrementOfTime} minutes.`, 'Disable');
+
+		}, timeIncrement)
+
+	} else if(response === 'No'){ 
+		vscode.window.showInformationMessage('Let\'s try again tomorrow!');
+	}
 
 	console.log('Congratulations, your extension "be-human" is now active!');
 
 	let disposable = vscode.commands.registerCommand('be-human.helloWorld', function () {
 
-		vscode.window.showInformationMessage('Hello World from be-human!');
+		vscode.window.showInformationMessage('Welcome to beHuman!');
 	});
 
 	let trackTime = vscode.commands.registerCommand('be-human.trackTime', function () {
@@ -28,8 +45,8 @@ function activate(context) {
 			const incrementOfTime = Math.round((lapTime - startTime) / 60000);
 			
 
-			vscode.window.showInformationMessage(`You have been working for ${incrementOfTime} minutes.`);
-		}, hour)
+			vscode.window.showInformationMessage(`You have been working for ${incrementOfTime} minutes. (This pops up every three seconds...Enjoy!)`);
+		}, 3000)
 	})
 
 	context.subscriptions.push(disposable);
