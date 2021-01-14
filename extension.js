2f5a07cae3c96d5b33ad019a181f8a2d1936081c
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-01-14 13:19:49 -0500

    add time tracking command and initialize start time with one hour increment

--- extension.js
+++ extension.js
@@ -1,34 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const { start } = require('repl');
+const vscode = require('vscode');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	const hour = (1000 * 60 * 60);
+
+	const startTime = new Date();
+	const localTime = startTime.toLocaleTimeString();
+	
+
 	console.log('Congratulations, your extension "be-human" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('be-human.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from be-human!');
 	});
 
+	let trackTime = vscode.commands.registerCommand('be-human.trackTime', function () {
+
+		setInterval(() => {
+			const lapTime = new Date();
+
+			const incrementOfTime = Math.round((lapTime - startTime) / 60000);
+			
+
+			vscode.window.showInformationMessage(`You have been working for ${incrementOfTime} minutes.`);
+		}, hour)
+	})
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(trackTime);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
