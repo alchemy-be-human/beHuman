1a433ddbe11250df00b164fa453d500e3cd2246f
Franco Ortega <francopdx@gmail.com>
2021-01-14 15:07:26 -0800

    add tips table

--- __tests__/tips.test.js
+++ __tests__/tips.test.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 //pool path may need to be double-checked
-const pool = require('..lib/utils/pool');
+const pool = require('../lib/utils/pool');
 
 describe('tests for model 2', () => {
     beforeEach(() => pool.query(fs.readFileSync('.sql/setup.sql', 'utf-8')));
