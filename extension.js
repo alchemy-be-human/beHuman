e48700647e545d354871f044ba17b4ffc0492268
Rhone Lachner <fernandclay@gmail.com>
2021-01-19 14:21:21 -0800

    updated reset fuction with Ryans help

--- extension.js
+++ extension.js
@@ -12,58 +12,66 @@ const activate = async (context) => {
 	const timeInterval = Number(vscode.workspace.getConfiguration("be-human").get("timeInterval"))
 			// changed 15 to "timeInterval"
 	const timeIncrement = (oneMinute * timeInterval);
-	let startTime = new Date();
-	const localTime = startTime.toLocaleTimeString();
+	let startTime = Date.now();
+	let intervalId = null;
+	// const localTime = startTime.toLocaleTimeString();
 
 	const response = await vscode.window.showInformationMessage('Welcome to beHuman! Would you like to be reminded to take breaks today?', 'Yes', 'No');
 	
 	if(response === 'Yes') {
 		vscode.window.showInformationMessage('Have a great day!');
-		
-		setInterval(() => {
-			const lapTime = new Date();
+		intervalId = setInterval(() => {
+			const lapTime = Date.now();
 			const incrementOfTime = Math.round((lapTime - startTime) / 60000);
+			vscode.window.showInformationMessage(`You have been working for ${incrementOfTime} minutes.`, 'Disable');
+		}, timeIncrement)
+	} else if(response === 'No'){ 
+		vscode.window.showInformationMessage('Let\'s try again tomorrow!');
+	}
+	let disposable = vscode.commands.registerCommand('be-human.helloWorld', function () {
+		vscode.window.showInformationMessage('Welcome to beHuman!');
+	});
 
+//USED FOR TESTING ONLY????
+	// let trackTime = vscode.commands.registerCommand('be-human.trackTime', function () {
+
+	// setInterval(() => {
+	// 		const lapTime = new Date();
+	// 		const incrementOfTime = Math.round((lapTime - startTime) / 60000);
+
+	// 		vscode.window.showInformationMessage(`You have been working for ${incrementOfTime} minutes. (This pops up every three seconds...Enjoy!)`);
+	// 	}, 3000)
+	// })
+
+	    // THIS IS WHERE STOP WATCH USER COMMAND FUNCTIONS BEGIN
+
+    let resetTimer = vscode.commands.registerCommand('be-human.resetTime', function () {
+		clearInterval(intervalId);
+
+		intervalId = setInterval(() => {
+			const lapTime = Date.now();
+			const incrementOfTime = Math.round((lapTime - startTime) / 60000);
 			vscode.window.showInformationMessage(`You have been working for ${incrementOfTime} minutes.`, 'Disable');
 
 		}, timeIncrement)
 
-	} else if(response === 'No'){ 
-		vscode.window.showInformationMessage('Let\'s try again tomorrow!');
-	}
-
-	console.log('Congratulations, your extension "be-human" is now active!');
-
-	let disposable = vscode.commands.registerCommand('be-human.helloWorld', function () {
-
-		vscode.window.showInformationMessage('Welcome to beHuman!');
-	});
-
-	let trackTime = vscode.commands.registerCommand('be-human.trackTime', function () {
-
-		setInterval(() => {
-			const lapTime = new Date();
-			const incrementOfTime = Math.round((lapTime - startTime) / 60000);
-
-			vscode.window.showInformationMessage(`You have been working for ${incrementOfTime} minutes. (This pops up every three seconds...Enjoy!)`);
-		}, 3000)
-	})
-
-	    // THIS IS WHERE STOP WATCH USER COMMAND FUNCTIONS BEGIN
-
-    let resetTime = vscode.commands.registerCommand('be-human.resetTime', function () {
-		 startTime = new Date();
-		 return resetTime
+		 return resetTimer
     });
 
-    // const pauseStart = 
-    // const pauseStop = 
+	// let stopTimer = vscode.commands.registerCommand('be-human.startTime', function () {
+	// 	startTime = new Date();
+		
+	// 	return stopTimer
+	// });
 
+	//  let startTimer = vscode.commands.registerCommand('be-human.startTime', function () {
+	// 		startTime = new Date();
 
+	// 		return startTimer
+	// 	});
 	context.subscriptions.push(disposable);
-	context.subscriptions.push(trackTime);
+	context.subscriptions.push(resetTimer);
 }
-exports.activate = activate;
 
 function deactivate() {}
 
