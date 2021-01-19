d4dffa024a3c02bc33cbf4acae3d2abf584afe1f
Bryana Kitchen <67290473+bryanakitchen@users.noreply.github.com>
2021-01-19 18:11:07 -0500

    add convertTime function (#10)
    * add convertTime function
    
    * Update convert-time.js

---
+++ utils/convert-time.js
@@ -0,0 +1,17 @@
+function convertTime(num) {
+    const hours = num / 60;
+    const roundHours = Math.floor(hours);
+    const minutes = (hours - roundHours) * 60;
+    const roundMinutes = Math.round(minutes);
+
+    if (number < 60) {
+        return `${roundMinutes} minutes`
+    } else if (number === 60) {
+        return `${hours} hour`;
+    } else if (number > 60) {
+        return `${roundHours} hours and ${roundMinutes} minutes`
+    }
+
+  }
+
+  module.exports = convertTime;
