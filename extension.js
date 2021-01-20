70545003a6489f7f3093dec317c3309e6ab9697f
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-01-20 17:43:14 -0500

    update timeInterval option

--- extension.js
+++ extension.js
@@ -11,7 +11,7 @@ const activate = async (context) => {
 
 	const timeInterval = Number(vscode.workspace.getConfiguration("be-human").get("timeInterval"))
 
-	const timeIncrement = (oneMinute * timeInterval);
+	const userTimeInterval = (oneMinute * timeInterval);
 
 	let startTime = Date.now();
 	let intervalId = null;
@@ -36,7 +36,7 @@ const activate = async (context) => {
 				const randomLink = await fetch.get('http://be-human-demo-staging.herokuapp.com/api/v1/links/random')
 				vscode.env.openExternal(vscode.Uri.parse(`${randomLink.body.url}`));
 			}
-		}, 600000)
+		}, userTimeInterval)
 
 	} else if(response === 'No'){ 
 		vscode.window.showInformationMessage('Let\'s try again tomorrow!');
@@ -60,11 +60,10 @@ const activate = async (context) => {
 				const randomLink = await fetch.get('http://be-human-demo-staging.herokuapp.com/api/v1/links/random')
 				vscode.env.openExternal(vscode.Uri.parse(`${randomLink.body.url}`));
 			}
-		}, 600000)
+		}, userTimeInterval)
 
     });
 
-	// context.subscriptions.push(disposable);
 	context.subscriptions.push(resetTimer);
 }
 
